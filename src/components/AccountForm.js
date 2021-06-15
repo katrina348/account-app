2353765010d893ea869395117527015a3a54cc55
Kat Duff <katrina348@gmail.com>
2021-06-15 14:29:32 -0600

    routes working

---
+++ src/components/AccountForm.js
@@ -0,0 +1,8 @@
+const AccountForm = () => {
+  return (
+    <div>
+      <h1>AccountForm</h1>
+    </div>
+  )
+}
+export default AccountForm
---
+++ src/components/AccountProfile.js
@@ -0,0 +1,13 @@
+import Account from "./Account"
+import AccountForm from "./AccountForm"
+
+const AccountProfile = () => {
+  return (
+    <div>
+      <h1>AccountProfile</h1>
+      <Account/>
+      <AccountForm/>
+    </div>
+  )
+}
+export default AccountProfile
